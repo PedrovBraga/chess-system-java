7365bc7ef0fdecc014f08cd68fc472c826355d17
Pedro Braga <bragavpedro@hotmail.com>
2020-04-21 13:14:40 -0300

    Start to implement Board and Piece Real

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@
  */
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 import javafx.geometry.Pos;
 
@@ -19,8 +20,7 @@ public class Program {
      */
     public static void main(String[] args) {
         
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board[8][8];
         
         
         
